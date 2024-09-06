689e78c69e5d04adae1c62ecf80bbeb731aebe00
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-09-06 13:50:54 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@styles/globals.css";
@@ -15,7 +16,10 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="de">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
