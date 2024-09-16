c0a796bb999eadd90ecd5c754f242db09274ca6f
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-09-17 00:14:48 +0200

    change position

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center mt-40 ml-10 font-display">
+    <div className="flex items-center mt-10 font-display">
       <button
         onClick={handleClick}
         className="px-8 py-8 bg-beige text-grafit font-semibold hover:text-gold focus:outline-none focus:shadow-outline transition-colors duration-300 text-4xl"
