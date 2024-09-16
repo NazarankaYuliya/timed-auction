c63fefe3f061099f721c23deb749e0d5cbfb81ef
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-09-17 00:08:26 +0200

    change button color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         gold: "#ceb27a",
         grafit: "#454d4f",
         "light-rose": "#fbf6f3",
-        beige: "#f2f0ef",
+        beige: "#fbf6f3",
       },
     },
   },
