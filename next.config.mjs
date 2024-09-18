c675ed9a590fb11e7d156da2c6d965cca5eec26c
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-09-18 19:22:06 +0200

    change admin cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/admin/**",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=60",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
