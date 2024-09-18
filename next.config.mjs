d283eeda941271f30d1cea6e2fd259fbf9ace0c4
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-09-18 19:12:35 +0200

    change cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*).(jpg|jpeg|png|webp|gif)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
