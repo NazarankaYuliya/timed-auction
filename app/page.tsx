2669523ac2137c4105302daaf1f10eede5235e31
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-09-22 22:06:34 +0200

    roll back

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import PlausibleProvider from "next-plausible";
 
 export default function Home() {
   const handleClick = () => {
@@ -7,15 +6,13 @@ export default function Home() {
   };
 
   return (
-    <PlausibleProvider domain="timed-auction.vercel.app">
-      <div className="flex items-center mt-10 font-display">
-        <button
-          onClick={handleClick}
-          className="px-8 py-8 bg-beige text-grafit hover:text-gold focus:outline-none focus:shadow-outline transition-colors duration-300 text-2xl sm:text-4xl leading-5"
-        >
-          zur Online Auktion
-        </button>
-      </div>
-    </PlausibleProvider>
+    <div className="flex items-center mt-10 font-display">
+      <button
+        onClick={handleClick}
+        className="px-8 py-8 bg-beige text-grafit hover:text-gold focus:outline-none focus:shadow-outline transition-colors duration-300 text-2xl sm:text-4xl leading-5"
+      >
+        zur Online Auktion
+      </button>
+    </div>
   );
 }
