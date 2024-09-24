6a7d6279e486447b40ea8a18c1f71cd3dd4c48c2
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-09-24 17:17:21 +0200

    change images config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    minimumCacheTTL: 2592000,
     formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 420, 768, 1024, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
